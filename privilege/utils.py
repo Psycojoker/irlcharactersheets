8fb51462b89c889f029ac3b22bee20f80e2c5597
Laurent Peuch <cortex@worlddomination.be>
2014-04-01 10:42:58 +0200

    [mod] put content in data all the time

--- privilege/utils.py
+++ privilege/utils.py
@@ -8,6 +8,6 @@ def get_categories():
         cat["title"] = category.title.text if category.title else category.name.capitalize()
         cat["type"] = category["type"]
         cat["name"] = category.name
-        cat["options"] = [{"value": x.name, "title": x.title.text} for x in category.options.childGenerator() if x and x != "\n"]
+        cat["options"] = [{"value": x.name, "title": x.title.text, "content": x.content.text} for x in category.options.childGenerator() if x and x != "\n"]
         data.append(cat)
     return data
