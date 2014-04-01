3d454b5fe4836096026d92688fe4ee335d09d466
Laurent Peuch <cortex@worlddomination.be>
2014-04-01 11:22:31 +0200

    [mod] trololol

--- privilege/urls.py
+++ privilege/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('privilege.views',
     url(r'^$', 'index', name='index'),
+    url(r'^sheet/$', 'sheet', name='sheet'),
 )
--- privilege/views.py
+++ privilege/views.py
@@ -4,3 +4,8 @@ from .utils import get_categories
 
 def index(request):
     return render(request, "index.haml", {"privileges": get_categories()})
+
+
+def sheet(request):
+    print request.GET
+    return render(request, "sheet.haml")
